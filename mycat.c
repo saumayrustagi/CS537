3d404638acaaa3e0707cb9e2fc0b17f19f5cb7a1
Saumay Rustagi <saumay03pro@gmail.com>
2024-01-08 16:51:03 +0530

    Reformat mycat.c

--- mycat.c
+++ mycat.c
@@ -2,20 +2,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	int fd;
 	char buf[2048];
 	ssize_t nread;
 
-	if ((fd = open(argv[1], O_RDONLY)) == -1){
+	if ((fd = open(argv[1], O_RDONLY)) == -1)
+	{
 		return -1;
 	}
 
-	while ((nread = read(fd, buf, sizeof buf)) > 0){
+	while ((nread = read(fd, buf, sizeof buf)) > 0)
+	{
 		ssize_t total_written = 0;
-		while (total_written < nread){
+		while (total_written < nread)
+		{
 			ssize_t n_written = write(STDOUT_FILENO, buf + total_written, nread - total_written);
-			if (n_written < 1) return 1;
+			if (n_written < 1)
+				return 1;
 			total_written += n_written;
 		}
 	}
